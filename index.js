31c1889f65df5c78801dae6d6601535efb13ad6d
Byron Sommardahl <byron@acklenavenue.com>
2017-04-16 18:36:44 -0600

    Added more detail and db interation.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var statusService = require('./statusService');
+var StatusService = require('./statusService');
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser')
@@ -7,18 +7,44 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
-    statusService.getMostRecent()
-        .then((status) => {
-            res.render('home', status);
-        });
-});
 
-app.post('/', (req, res) => {
-    statusService.new(req.body);
-});
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.MONGODB_URI;
+MongoClient.connect(url, function(err, db) {
+    console.log("Mongo database connected.");
 
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-    console.log(`Example app listening on port ${port}!`);
+    var col = db.collection('history');
+    var aggregate = (query, callback) => {
+        col.aggregate(query, callback);
+    };
+    var insertOne = (payload, callback) => {
+        col.insertOne(payload, callback);
+    }
+
+    var statusService = new StatusService(aggregate, insertOne);
+
+    app.get('/', function (req, res) {
+        statusService.getMostRecent()
+            .then((status) => {
+                res.render('home', status);
+            })
+            .catch((err) => {
+                res.status(500).send({error: err});
+            });
+    });
+
+    app.post('/', (req, res) => {
+        statusService.new(req.body)
+            .then(()=>{
+                res.status(200).end();
+            })
+            .catch((err) => {
+                res.status(400).send({error: err});
+            });
+    });
+
+    var port = process.env.PORT || 3000;
+    app.listen(port, function () {
+        console.log(`Example app listening on port ${port}!`);
+    });
 });
--- statusService.js
+++ statusService.js
@@ -1,240 +1,56 @@
 
-module.exports = {
-    save: (payload) => {
-        console.log(payload);
-    },
-    getMostRecent: () => {
-        return Promise.resolve([{
-            source: '00:0c:29:27:09:3a',
-            status: 'ok',
-            time: '2017-04-16T22:45:57.844Z',
-            config: {
-                cronTime: '00 30 11 * * 1-5',
-                timeZone: 'America/Chicago'
+module.exports = function(aggregate, insertOne){
+    {
+        return {
+            new: (payload) => {
+                return new Promise((resolve, reject) => {
+                    if(!payload.source || !payload.targets) {
+                        reject("Invalid payload. Properties 'source' and 'targets' required.");
+                    }
+                    insertOne(payload, function(err, result) {
+                        if(err) {
+                            reject(err)
+                        }
+                        else{
+                            resolve();
+                        }
+                    });
+                });
             },
-            targets: [
-                {
-                    host: '192.168.1.1',
-                    alive: true,
-                    output: 'PING 192.168.1.1 (192.168.1.1) 56(84) bytes of data.\n64 bytes from 192.168.1.1: icmp_seq=1 ttl=128 time=3.74 ms\n\n--- 192.168.1.1 ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 3.745/3.745/3.745/0.000 ms\n',
-                    time: 3.74,
-                    min: '3.745',
-                    max: '3.745',
-                    avg: '3.745',
-                    stddev: '0.000',
-                    numeric_host: '192.168.1.1',
-                    name: 'Byron\'s Firewall'
-                },
-                {
-                    host: '192.168.1.50',
-                    alive: true,
-                    output: 'PING 192.168.1.50 (192.168.1.50) 56(84) bytes of data.\n64 bytes from 192.168.1.50: icmp_seq=1 ttl=128 time=4.84 ms\n\n--- 192.168.1.50 ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 4.844/4.844/4.844/0.000 ms\n',
-                    time: 4.84,
-                    min: '4.844',
-                    max: '4.844',
-                    avg: '4.844',
-                    stddev: '0.000',
-                    numeric_host: '192.168.1.50',
-                    name: 'Olwing\'s Router'
-                },
-                {
-                    host: '192.168.50.1',
-                    alive: true,
-                    output: 'PING 192.168.50.1 (192.168.50.1) 56(84) bytes of data.\n64 bytes from 192.168.50.1: icmp_seq=1 ttl=128 time=5.37 ms\n\n--- 192.168.50.1 ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 5.379/5.379/5.379/0.000 ms\n',
-                    time: 5.37,
-                    min: '5.379',
-                    max: '5.379',
-                    avg: '5.379',
-                    stddev: '0.000',
-                    numeric_host: '192.168.50.1',
-                    name: 'Tegucigalpa'
-                },
-                {
-                    host: 'google.com',
-                    alive: true,
-                    output: 'PING google.com (172.217.7.142) 56(84) bytes of data.\n64 bytes from 172.217.7.142: icmp_seq=1 ttl=128 time=57.8 ms\n\n--- google.com ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 57.833/57.833/57.833/0.000 ms\n',
-                    time: 57.8,
-                    min: '57.833',
-                    max: '57.833',
-                    avg: '57.833',
-                    stddev: '0.000',
-                    numeric_host: '172.217.7.142',
-                    name: 'google.com'
-                }]
-            },
-            {
-            source: '00:0c:29:27:09:3b',
-            time: '2017-04-16T22:45:57.844Z',
-            status: 'ok',
-            config: {
-                cronTime: '00 30 11 * * 1-5',
-                timeZone: 'America/Chicago'
-            },
-            targets: [
-                {
-                    host: '192.168.1.1',
-                    alive: true,
-                    output: 'PING 192.168.1.1 (192.168.1.1) 56(84) bytes of data.\n64 bytes from 192.168.1.1: icmp_seq=1 ttl=128 time=3.74 ms\n\n--- 192.168.1.1 ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 3.745/3.745/3.745/0.000 ms\n',
-                    time: 3.74,
-                    min: '3.745',
-                    max: '3.745',
-                    avg: '3.745',
-                    stddev: '0.000',
-                    numeric_host: '192.168.1.1',
-                    name: 'Byron\'s Firewall'
-                },
-                {
-                    host: '192.168.1.50',
-                    alive: true,
-                    output: 'PING 192.168.1.50 (192.168.1.50) 56(84) bytes of data.\n64 bytes from 192.168.1.50: icmp_seq=1 ttl=128 time=4.84 ms\n\n--- 192.168.1.50 ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 4.844/4.844/4.844/0.000 ms\n',
-                    time: 4.84,
-                    min: '4.844',
-                    max: '4.844',
-                    avg: '4.844',
-                    stddev: '0.000',
-                    numeric_host: '192.168.1.50',
-                    name: 'Olwing\'s Router'
-                },
-                {
-                    host: '192.168.50.1',
-                    alive: true,
-                    output: 'PING 192.168.50.1 (192.168.50.1) 56(84) bytes of data.\n64 bytes from 192.168.50.1: icmp_seq=1 ttl=128 time=5.37 ms\n\n--- 192.168.50.1 ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 5.379/5.379/5.379/0.000 ms\n',
-                    time: 5.37,
-                    min: '5.379',
-                    max: '5.379',
-                    avg: '5.379',
-                    stddev: '0.000',
-                    numeric_host: '192.168.50.1',
-                    name: 'Tegucigalpa'
-                },
-                {
-                    host: 'google.com',
-                    alive: true,
-                    output: 'PING google.com (172.217.7.142) 56(84) bytes of data.\n64 bytes from 172.217.7.142: icmp_seq=1 ttl=128 time=57.8 ms\n\n--- google.com ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 57.833/57.833/57.833/0.000 ms\n',
-                    time: 57.8,
-                    min: '57.833',
-                    max: '57.833',
-                    avg: '57.833',
-                    stddev: '0.000',
-                    numeric_host: '172.217.7.142',
-                    name: 'google.com'
-                }]
-            },
-            {
-            source: '00:0c:29:27:09:3c',
-            time: '2017-04-16T22:45:57.844Z',
-            status: 'ok',
-            config: {
-                cronTime: '00 30 11 * * 1-5',
-                timeZone: 'America/Chicago'
-            },
-            targets: [
-                {
-                    host: '192.168.1.1',
-                    alive: true,
-                    output: 'PING 192.168.1.1 (192.168.1.1) 56(84) bytes of data.\n64 bytes from 192.168.1.1: icmp_seq=1 ttl=128 time=3.74 ms\n\n--- 192.168.1.1 ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 3.745/3.745/3.745/0.000 ms\n',
-                    time: 3.74,
-                    min: '3.745',
-                    max: '3.745',
-                    avg: '3.745',
-                    stddev: '0.000',
-                    numeric_host: '192.168.1.1',
-                    name: 'Byron\'s Firewall'
-                },
-                {
-                    host: '192.168.1.50',
-                    alive: true,
-                    output: 'PING 192.168.1.50 (192.168.1.50) 56(84) bytes of data.\n64 bytes from 192.168.1.50: icmp_seq=1 ttl=128 time=4.84 ms\n\n--- 192.168.1.50 ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 4.844/4.844/4.844/0.000 ms\n',
-                    time: 4.84,
-                    min: '4.844',
-                    max: '4.844',
-                    avg: '4.844',
-                    stddev: '0.000',
-                    numeric_host: '192.168.1.50',
-                    name: 'Olwing\'s Router'
-                },
-                {
-                    host: '192.168.50.1',
-                    alive: true,
-                    output: 'PING 192.168.50.1 (192.168.50.1) 56(84) bytes of data.\n64 bytes from 192.168.50.1: icmp_seq=1 ttl=128 time=5.37 ms\n\n--- 192.168.50.1 ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 5.379/5.379/5.379/0.000 ms\n',
-                    time: 5.37,
-                    min: '5.379',
-                    max: '5.379',
-                    avg: '5.379',
-                    stddev: '0.000',
-                    numeric_host: '192.168.50.1',
-                    name: 'Tegucigalpa'
-                },
-                {
-                    host: 'google.com',
-                    alive: true,
-                    output: 'PING google.com (172.217.7.142) 56(84) bytes of data.\n64 bytes from 172.217.7.142: icmp_seq=1 ttl=128 time=57.8 ms\n\n--- google.com ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 57.833/57.833/57.833/0.000 ms\n',
-                    time: 57.8,
-                    min: '57.833',
-                    max: '57.833',
-                    avg: '57.833',
-                    stddev: '0.000',
-                    numeric_host: '172.217.7.142',
-                    name: 'google.com'
-                }]
-            },
-            {
-            source: '00:0c:29:27:09:3d',
-            time: '2017-04-16T22:45:57.844Z',
-            status: 'ok',
-            config: {
-                cronTime: '00 30 11 * * 1-5',
-                timeZone: 'America/Chicago'
-            },
-            targets: [
-                {
-                    host: '192.168.1.1',
-                    alive: true,
-                    output: 'PING 192.168.1.1 (192.168.1.1) 56(84) bytes of data.\n64 bytes from 192.168.1.1: icmp_seq=1 ttl=128 time=3.74 ms\n\n--- 192.168.1.1 ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 3.745/3.745/3.745/0.000 ms\n',
-                    time: 3.74,
-                    min: '3.745',
-                    max: '3.745',
-                    avg: '3.745',
-                    stddev: '0.000',
-                    numeric_host: '192.168.1.1',
-                    name: 'Byron\'s Firewall'
-                },
-                {
-                    host: '192.168.1.50',
-                    alive: true,
-                    output: 'PING 192.168.1.50 (192.168.1.50) 56(84) bytes of data.\n64 bytes from 192.168.1.50: icmp_seq=1 ttl=128 time=4.84 ms\n\n--- 192.168.1.50 ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 4.844/4.844/4.844/0.000 ms\n',
-                    time: 4.84,
-                    min: '4.844',
-                    max: '4.844',
-                    avg: '4.844',
-                    stddev: '0.000',
-                    numeric_host: '192.168.1.50',
-                    name: 'Olwing\'s Router'
-                },
-                {
-                    host: '192.168.50.1',
-                    alive: true,
-                    output: 'PING 192.168.50.1 (192.168.50.1) 56(84) bytes of data.\n64 bytes from 192.168.50.1: icmp_seq=1 ttl=128 time=5.37 ms\n\n--- 192.168.50.1 ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 5.379/5.379/5.379/0.000 ms\n',
-                    time: 5.37,
-                    min: '5.379',
-                    max: '5.379',
-                    avg: '5.379',
-                    stddev: '0.000',
-                    numeric_host: '192.168.50.1',
-                    name: 'Tegucigalpa'
-                },
-                {
-                    host: 'google.com',
-                    alive: true,
-                    output: 'PING google.com (172.217.7.142) 56(84) bytes of data.\n64 bytes from 172.217.7.142: icmp_seq=1 ttl=128 time=57.8 ms\n\n--- google.com ping statistics ---\n1 packets transmitted, 1 received, 0% packet loss, time 0ms\nrtt min/avg/max/mdev = 57.833/57.833/57.833/0.000 ms\n',
-                    time: 57.8,
-                    min: '57.833',
-                    max: '57.833',
-                    avg: '57.833',
-                    stddev: '0.000',
-                    numeric_host: '172.217.7.142',
-                    name: 'google.com'
-                }]
-            }]);
-    },
-};
+            getMostRecent: () => {
+                return new Promise((resolve, reject) => {
+                    aggregate([
+                        {$group: {
+                                "_id": "$source",
+                                name: {$last: '$name'},
+                                data: {$last: '$targets'},
+                                date: {$last: '$time'},
+                            }},
+                        {$project: {
+                                "source": "$_id",
+                                "targets": "$data",
+                                "name": "$name",
+                                "time": "$date"
+                            }},
+                        {$sort: {
+                                'source': 1,
+                                'date': -1
+                            }}
+                    ], function(err, result) {
+                        if(err){
+                            reject(err);
+                        }
+                        else{
+                            result = result.map((r) => {
+                                var allTargetsAlive = r.targets.every((t) => { return t.alive; });
+                                r.status = allTargetsAlive ? 'alive' : 'problem';
+                                return r;
+                            });
+                            resolve(result);
+                        }
+                    })
+                });
+            }
+        }
+    }
+}
